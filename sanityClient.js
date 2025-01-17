29694401839738cf1f6025347c4f45ee43687d27
atiasultani <160133182+atiasultani@users.noreply.github.com>
2025-01-17 18:41:32 +0500

    e-com practice

--- sanityClient.js
+++ sanityClient.js
@@ -4,9 +4,9 @@ exports.client = void 0;
 // sanityClient.ts
 var client_1 = require("@sanity/client");
 exports.client = (0, client_1.createClient)({
-    projectId: 'rohwei6u', // Replace with your project ID
+    projectId: 'lnokmotm', // Replace with your project ID
     dataset: 'production', // Or your dataset name
     apiVersion: '2024-01-04', // Today's date or latest API version
     useCdn: false, // Disable CDN for real-time updates
-    token: "skwAYL2EVT699VvWODU6xcibH3o0mwbKP9DoMrexRkfzRpaWfQahfSiwEuAlwZqU4FibHyEoVOR8gmHnwnDXW640gZU3mp1afdgil0yTqqL9Oqw4pqTssSZERXnLbXopBPVqpiW0OVN7hTAH0fF7GbSjAKDDk9kbYAAZZpDfdWhYAHWmtFAY"
+    token: 'skSHiancIjTQQSJZkza10oQitrAzH6sZNOE69TFshVMf9Qb9sXzrmwM7JQS0VmMNrxakb085ctIXmSoaFnKm9treCgLRTCibLp9ugwf2qNvZxTxYnK0eEGbUbW8H9C6ssMJkiaHADpxhPxsB9LQmmChN3QnXeLKbKeuRnYD2xUpfKDdZF9jm',
 });
--- sanityClient.ts
+++ sanityClient.ts
@@ -2,9 +2,9 @@
 import { createClient } from '@sanity/client';
 
 export const client = createClient({
-  projectId: process.env.projectId, // Replace with your project ID
+  projectId: 'lnokmotm', // Replace with your project ID
   dataset: 'production',        // Or your dataset name
   apiVersion: '2024-01-04',     // Today's date or latest API version
   useCdn: false,                // Disable CDN for real-time updates
-  token: process.env.token,
+  token: 'skSHiancIjTQQSJZkza10oQitrAzH6sZNOE69TFshVMf9Qb9sXzrmwM7JQS0VmMNrxakb085ctIXmSoaFnKm9treCgLRTCibLp9ugwf2qNvZxTxYnK0eEGbUbW8H9C6ssMJkiaHADpxhPxsB9LQmmChN3QnXeLKbKeuRnYD2xUpfKDdZF9jm',
 });
